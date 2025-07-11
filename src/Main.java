029e95e3260c934cde293d20d8a4f89188439092
Vishal Rawat <vishal.rawat@flydocs.aero>
2025-07-12 00:10:48 +0530

    feature: initial set up

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import com.vishu.algorithms.SortingAlgo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        SortingAlgo<Integer> algo = new SortingAlgo<Integer>();
+
+        // code
+        List<Integer> numbers = Arrays.asList(5, 2, 8, 1, 9);
+        algo.setArr(numbers);
+
+    }
+}
---
+++ src/com/vishu/algorithms/SortingAlgo.java
@@ -0,0 +1,17 @@
+package com.vishu.algorithms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingAlgo<T> {
+    private List<T> arr;
+
+    public List<T> getArr() {
+        return arr;
+    }
+
+    public void setArr(List<T> arr) {
+        this.arr = arr;
+    }
+}
